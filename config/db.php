128e835c166f4ea1df470f4a58028ce97e8679fe
Oleg Pustovalov <oleg@portalnews.ru>
2015-02-24 15:15:46 +0400

    Adding bootstrap themes

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=karnaval',
+    'dsn' => 'mysql:host=localhost;dbname=basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
--- config/web.php
+++ config/web.php
@@ -6,6 +6,8 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru',
+    'timeZone' => 'Europe/Moscow',    
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -37,6 +39,22 @@ $config = [
                 ],
             ],
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+//            'suffix' => '.html',
+//            'rules' => [
+//                // your rules go here
+//            ],
+        ],
+        'assetManager' => [
+            'bundles' => [
+                'yii\bootstrap\BootstrapAsset' => [
+                     'sourcePath' => '@app/bootstrapThemes/yeti',
+                     'css' => ['css/bootstrap.min.css']
+                ],
+            ]
+        ],        
         'db' => require(__DIR__ . '/db.php'),
     ],
     'params' => $params,
